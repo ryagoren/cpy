07966c853c84cbd18fff5a80c7e0d71937a23714
ryangeor <ryangeor@mainbox>
2024-07-28 17:04:27 +0100

    just use statics for maxsizes; read & encode in single go(s)

--- copy_osc52.c
+++ copy_osc52.c
@@ -27,11 +27,11 @@ int encode(char *buf, size_t len, char *encoded, size_t *read)
 		free(command);
 		return 1;
 	}
-	read = fread(encoded, 1, MAX-1, fp);
+	size_t bytes_read = fread(encoded, 1, MAX-1, fp);
 	*read=bytes_read;
 	pclose(fp);
 	if (bytes_read <=0) {
-		fprintf("encoding unsuccesssful. big sad.\n");
+		fprintf(stderr, "encoding unsuccesssful. big sad.\n");
 		return 1;
 	}
 	encoded[strcspn(encoded, "\n")] = 0;
@@ -47,82 +47,19 @@ int main()
 
 	size_t buf_remaining=MAX_USR;
 	size_t bytes_read;
-
 	bytes_read = fread(buf, 1, MAX_USR, stdin);
-	if bytes_read == 0 {
+	if (bytes_read == 0) {
 		fprintf(stderr, "error: input is empty (or errored, but im lazy), will not override.\n");
 		return 1;
 	}
 	buf[bytes_read]='\0';
 
 	size_t encode_pos;
-	int ret = encode(&buf, bytes_read, &encoded, &encode_pos);
+	int ret = encode(buf, bytes_read, encoded, &encode_pos);
 	if (ret != 0) {
-		fprintf("bigsad, failed to encode. wcyd.\n");
+		fprintf(stderr, "bigsad, failed to encode. wcyd.\n");
 		return ret;
 	}
 	output(encoded);
-	return 0;
-}
-
-
-int read_remainder(char *remainder)
-{
-	char buf[BUFSIZ];
-	ret = fread(buf, sizeof(*buf), sizeof(buf)/sizeof((buff)[0]), stdin);
-	if ret == 0 {
-		return 0;
-	}
-	size_t curlen = 0;
-	char *buf = malloc(bufsize);
-
-	if (!buf) {
-		fprintf(stderr, "malloc failed, there's some gooooooood sht happenin' on your box");
-		return 1;
-	}
-
-	int c;
-
-	while ((c=
-
-}
-
-
-
-
-int main(int argc, char **argv) {
-	if (argc != 1) {
-	fprintf(stderr, "-unimplemented!-\n");
-	return 1;
-	}
-
-	char buf[MAX]
-
-
-
-	char buf[BUFSIZ];
-	if (!fgets(buf, sizeof(buf), stdin)) {
-		fprintf(stderr, "failed to read from stdin\n");
-		return 1;
-	}
-
-	buf[strcspn(buf, "\n")] = 0;
-	char *command;
-	asprintf(&command, "echo -n \"%s\" | base64", buf);
-	FILE *fp = popen(command, "r");
-	if (fp == NULL) {
-		fprintf(stderr, "failed to encode provided text in base64\n");
-		free(command);
-		return 1;
-	}
-
-	char encoded[BUFSIZ];
-	fgets(encoded, sizeof(encoded), fp);
-	pclose(fp);
-
-	encoded[strcspn(encoded, "\n")] = 0;
-	output(encoded);
-
-	free(command);
 	return 0;
 }
