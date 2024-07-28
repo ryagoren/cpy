bd010b92bef738534e96a803f1ca62b364821cb6
ryangeor <ryangeor@mainbox>
2024-07-28 15:56:58 +0100

    simple init with fgets

--- copy_osc52.c
+++ copy_osc52.c
@@ -12,26 +12,36 @@ void output(char *encoded)
 
 int main(int argc, char **argv) {
 
-	fprintf(stderr, "wassup\n");
-
-
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s \"text to copy\"\n", argv[0]);
+	if (argc > 2) {
+		fprintf(stderr, "Usage: %s \"?text to copy\"\n", argv[0]);
 		return 1;
 	}
 
+	char buf[BUFSIZ];
+	if (!fgets(buf, sizeof(buf), stdin)) {
+		fprintf(stderr, "failed to read from stdin\n");
+		return 1;
+	}
+
+	buf[strcspn(buf, "\n")] = 0;
+
 	char *command;
-	asprintf(&command, "echo -n \"%s\" | base64", argv[1]);
+	asprintf(&command, "echo -n \"%s\" | base64", buf);
 
 	FILE *fp = popen(command, "r");
 	if (fp == NULL) {
 		fprintf(stderr, "failed to encode provided text in base64\n");
+		free(command);
 		return 1;
 	}
 
 	char encoded[BUFSIZ];
 	fgets(encoded, sizeof(encoded), fp);
 	pclose(fp);
+
 	encoded[strcspn(encoded, "\n")] = 0;
 	output(encoded);
+
+	free(command);
+	return 0;
 }
